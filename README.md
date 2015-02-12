282e7f67b85a0e96a27b3fb7304039612c3a9e2c
Jayme <kalkehcoisa@gmail.com>
2015-02-12 11:58:37 -0200

    Mais alguns ajustes no deploy.sh

--- README.md
+++ README.md
@@ -3,8 +3,20 @@
 
 # Materiais utilizados
 
- - Instalação: https://gist.github.com/hengxin/8e5040d7a8b354b1c82e
- - Uso básico (cqlsh): https://wiki.apache.org/cassandra/GettingStarted
+ - Instalação: 
+  - Cassandra:
+   - https://gist.github.com/hengxin/8e5040d7a8b354b1c82e
+  - Elastic Search:
+   - http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/setup-repositories.html
+
+ - Uso básico (cqlsh): 
+  - https://wiki.apache.org/cassandra/GettingStarted
  - Uso básico (cassandra-driver):
    - http://datastax.github.io/python-driver/getting_started.html
    - http://planetcassandra.org/getting-started-with-cassandra-and-python/
+
+ - Uso básico  (elasticsearch):
+  - http://joelabrahamsson.com/elasticsearch-101/
+  - http://www.elasticsearch.org/guide/en/kibana/current/using-kibana-for-the-first-time.html
+ - Uso básico  (lib python elasticsearch):
+  - http://blog.arisetyo.com/?p=332
--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,41 @@
-#instala o cassandra
-echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+#!/bin/bash
+
+if [ ! "$BASH_VERSION" ] ; then
+    exec /bin/bash "$0" "$@"
+fi
+
+USUARIO=$(who mom likes | awk '{print $1}')
+
+IS_ROOT=$(whoami)
+if [ "$IS_ROOT" == "root" ]; then
+    echo "Don't run as root/(using sudo)";
+    exit 0;
+fi
+
+#adiciona o repositorio do cassandra
+sudo touch /etc/apt/sources.list.d/cassandra.sources.list
+sudo add-apt-repository  "deb http://debian.datastax.com/community stable main"
 curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
+
+#adiciona o repositorio do elastic search
+wget -qO - https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo add-apt-repository "deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main"
+sudo update-rc.d elasticsearch defaults 95 10
 
 sudo apt-get update
 cat ./apt-reqs.txt | xargs sudo apt-get install -y
 
+pip install virtualenv
+
 #cria o virtualenv e instala as libs python nele
-virtualenv-2.7 /var/virtual_envs/simbiose
-source /var/virtual_envs/simbiose
-cat ./pip-reqs.txt | xargs pip install
+sudo mkdir -p /var/virtual_envs
+sudo mkdir -p /var/virtual_envs/simbiose
+sudo chown -R $USUARIO:$USUARIO /var/virtual_envs/
+
+virtualenv /var/virtual_envs/simbiose
+source /var/virtual_envs/simbiose/bin/activate
+pip install setuptools --upgrade
+pip install -r ./pip-reqs.txt 
+
+#cria o keyspace no cassandra
+cqlsh < create_db.cql
